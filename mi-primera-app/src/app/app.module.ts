23854cdf9b770c7c4b53479b38ef63de6ba01f0f
Arturo Barba <arturo.barba@iesdonana.org>
2019-01-22 23:52:09 +0100

    Pruebas

--- mi-primera-app/src/app/app.module.ts
+++ mi-primera-app/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AplicationInfoComponent } from './aplication-info/aplication-info.component';
+import { LoginCardComponent } from './login-card/login-card.component';
+import { LoginModule from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AplicationInfoComponent
+    AplicationInfoComponent,
+    LoginCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
