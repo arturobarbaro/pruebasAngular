db7aaee88c476f14ae3579d1a97f41485e9a94c2
Arturo Barba <arturo.barba@iesdonana.org>
2019-01-22 15:38:52 +0100

    Pruebas de TypeScript

---
+++ typescript/main.js
@@ -0,0 +1,5 @@
+"use strict";
+maxValue(2, 1);
+function maxValue(value1, value2) {
+    return Math.max(value1, value2);
+}
---
+++ typescript/main.ts
@@ -0,0 +1,8 @@
+maxValue(2,1);
+
+function maxValue(value1: number, value2: number){
+    return Math.max(value1,value2)
+}
+
+// Enums
+enum Color {Red, Green, Yellow};
