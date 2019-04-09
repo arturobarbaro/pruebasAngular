9e912fbd8c40fffec598ba6fedbef9a60bbded4e
Arturo Barba Rodríguez <abarba@atsistemas.com>
2019-04-09 16:24:16 +0200

    Inicial

--- redux/01-redux-basic/app-1.ts
+++ redux/01-redux-basic/app-1.ts
@@ -1,1 +1,26 @@
 console.log('Buenas');
+
+ // Acciones
+ interface Action {
+   type: string;
+   payload?: any;
+ }
+
+// const incrementadorAction: Action = {
+//   type: 'INCREMENTAR'
+// }
+
+function reducer( state = 10, action: Action ) {
+  if ( action.type == 'INCREMENTAR') {
+    return state++;
+  }
+  return state;
+}
+
+//Usar el reducer
+
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+};
+
+console.log(reducer(10,incrementadorAction));
