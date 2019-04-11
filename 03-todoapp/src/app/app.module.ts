cfab669df0b46961855f7b23251003fbcd9ef6c8
Arturo Barba Rodríguez <abarba@atsistemas.com>
2019-04-11 16:32:21 +0200

    Components

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodosListComponent } from './todo/todos-list/todos-list.component';
+import { TodoItemComponent } from './todo/todo-item/todo-item.component';
+import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TodoComponent,
+    TodosListComponent,
+    TodoItemComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule
